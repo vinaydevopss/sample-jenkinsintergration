293963a267ebb6c186feeed4d4e74b34de83a6a9
vinaydevopss <35803169+vinaydevopss@users.noreply.github.com>
2025-06-25 20:23:22 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
